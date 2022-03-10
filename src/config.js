4679510388da3fe45663137eb54722f1b916b09e
Andy Kay <andykay.me@gmail.com>
2022-03-09 21:37:14 -0500

    more artwork tweaks

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 100,
+    growEditionSizeTo: 5000,
     layersOrder: [
       { name: "Background" },
       { name: "Type" },
@@ -48,7 +48,7 @@ const layerConfigurations = [
     ],
   },
   {
-    growEditionSizeTo: 100,
+    growEditionSizeTo: 6969,
     layersOrder: [
       { name: "Background" },
       { name: "Type" },
